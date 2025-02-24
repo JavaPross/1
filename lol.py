000f5ceb900ba48653026ca09df92e9668068ba3
JavaP <javad.huseynov2307@gmail.com>
2025-02-24 12:26:46 +0400

    Update lol.py

--- lol.py
+++ lol.py
@@ -1,1 +1,15 @@
-l
+class IterableWithGenerator:
+    def __init__(self, start, stop):
+        self.start = start
+        self.stop = stop
+
+    def __iter__(self):
+        def generator():
+            for i in range(self.start, self.stop):
+                yield i
+        return generator()
+
+iterable = IterableWithGenerator(1, 5)
+
+for value in iterable:
+    print(value)
